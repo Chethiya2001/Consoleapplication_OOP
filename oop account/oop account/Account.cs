84a49455bd0ec4a26599c5ce0010bfdb39687fa5
Chethiya2001 <chethiyabandaraherath@gmail.com>
2022-08-05 11:42:52 +0530

    OOP example

---
+++ oop account/oop account/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_account
+{
+    class Account
+    {
+        string accNo, name;
+        int balance;
+
+        public Account()
+        {
+            Console.WriteLine("Executed blank construtor");
+        } 
+        public Account(string ano, string na) 
+        {
+            this.accNo= ano;
+            this.name= na;
+        }
+        public Account(string ano, string na, int bal)
+        {
+            this.accNo = ano;
+            this.name = na;
+            this.balance = bal;
+
+        }
+        public float calInterest()
+        {
+            float interest;
+            interest = this.balance * 10 / 100;
+            return interest;
+
+        }
+
+ 
+    
+
+        public void showData()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Account Number : " + this.accNo);
+            Console.WriteLine("Account Name : " + this.name);
+            Console.WriteLine("Account Number : " + this.balance);
+            
+        }
+        public void deposit(int amt)
+        {
+            this.balance = this.balance + amt;
+        }
+        public void withdraw(int amt)
+        {
+            this.balance = this.balance - amt;
+        }
+        
+    }
+}
---
+++ oop account/oop account/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //createing object
+
+            Account a1 = new Account("1","Tom");
+            Account a2 = new Account("2","Bob",80000);
+            Account a3 = new Account("3", "jhon", 200000);
+            Account a4 = new Account();
+
+            a1.deposit(1000);
+            a1.showData();
+            a2.deposit(2000);
+            a2.showData();
+            a3.showData();
+            float z = a2.calInterest();
+            Console.WriteLine("Interest is :" + z);
+            a4.showData();
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
